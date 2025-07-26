3106dcee89574a863a130a307b2dc13f7ea406a7
iamjjanga-ouo <namils147@kakao.com>
2025-07-26 16:11:10 +0900

    2주차 : Init Vagrantfile & init_scripts (kubernetes version 받는 부분도 추가)

---
+++ study/w2/init_scripts/k8s-w.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo ">>>> K8S Node config Start <<<<"
+
+echo "[TASK 1] K8S Controlplane Join"
+curl --silent -o /root/kubeadm-join-worker-config.yaml https://raw.githubusercontent.com/iamjjanga-ouo/cilium-lab/refs/heads/main/study/w2/kubeadm-configs/kubeadm-join-worker-config.yaml
+NODEIP=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+sed -i "s/NODE_IP_PLACEHOLDER/${NODEIP}/g" /root/kubeadm-join-worker-config.yaml
+kubeadm join --config="/root/kubeadm-join-worker-config.yaml" > /dev/null 2>&1
+
+
+echo ">>>> K8S Node config End <<<<"
